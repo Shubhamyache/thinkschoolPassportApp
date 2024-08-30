4443b1edff2abeddde97c0504203298c7ec788b4
shubhamyache <yacheshubham100@gmail.com>
2024-08-30 12:08:22 +0530

    config changes

--- PassportApplicationWebApi/PassportApplicationWebApi/obj/Release/net8.0/PassportApplicationWebApi.AssemblyInfo.cs
+++ PassportApplicationWebApi/PassportApplicationWebApi/obj/Release/net8.0/PassportApplicationWebApi.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("PassportApplicationWebApi")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16965cae1614b9580b00caf4582ec98071534ced")]
 [assembly: System.Reflection.AssemblyProductAttribute("PassportApplicationWebApi")]
